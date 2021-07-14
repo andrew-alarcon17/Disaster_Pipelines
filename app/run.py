4770cfba4531a2ceac2cac4027062b07d3a4b9d5
Andrew Alarcon <35975047+andrew-alarcon17@users.noreply.github.com>
2021-07-14 14:46:13 -0400

    Add files via upload

---
+++ app/run.py
@@ -0,0 +1,133 @@
+import json
+import plotly
+import pandas as pd
+import nltk
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk import pos_tag, word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+app = Flask(__name__)
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    """
+    
+    """
+    def starting_verb(self, text):
+        # tokenize by sentences
+        sentence_list = nltk.sent_tokenize(text)
+        
+        for sentence in sentence_list:
+            # tokenize each sentence into words and tag part of speech
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            
+            # index pos_tags to get the first word and part of speech tag
+            first_word, first_tag = pos_tags[0]
+            
+            # return true if the first word is an appropriate verb or RT for retweet
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+
+                return True
+        return False
+    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
+def tokenize(text):
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+# load data
+#engine = create_engine('sqlite:///../data/YourDatabaseName.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+#df = pd.read_sql_table('YourTableName', engine)
+df = pd.read_sql_table('df', engine)
+
+# load model
+model = joblib.load("../models/classifier.pkl")
+
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    # extract data needed for visuals
+    # TODO: Below is an example - modify to extract data for your own visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    
+    # create visuals
+    # TODO: Below is an example - modify to create your own visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        }
+    ]
+    
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
